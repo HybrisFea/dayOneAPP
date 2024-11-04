d416cbcc7529d8ef7aa3982be6ec750b89214f0d
antoniochiostro <antonio.chiostro@capgemini.com>
2024-11-04 16:45:07 +0100

    add fix

--- src/pages/PianoAlimentare.js
+++ src/pages/PianoAlimentare.js
@@ -3,8 +3,61 @@ import React from "react";
 function PianoAlimentare() {
   return (
     <div>
-      <h1>Piano Alimentare</h1>
-      <p>Qui verrà mostrato il tuo piano alimentare.</p>
+      <h1>Piano Alimentare Settimanale</h1>
+      <table
+        style={{ width: "100%", borderCollapse: "collapse", marginTop: "20px" }}
+      >
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #ccc", padding: "8px" }}>Giorno</th>
+            <th style={{ border: "1px solid #ccc", padding: "8px" }}>Pranzo</th>
+            <th style={{ border: "1px solid #ccc", padding: "8px" }}>Cena</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Lunedì</td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Tacchino
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Pollo</td>
+          </tr>
+          <tr>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Martedì
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Maiale</td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Merluzzo
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Mercoledì
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Pollo</td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Uova</td>
+          </tr>
+          <tr>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Giovedì
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Uova</td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>Tonno</td>
+          </tr>
+          <tr>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Venerdì
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Merluzzo
+            </td>
+            <td style={{ border: "1px solid #ccc", padding: "8px" }}>
+              Carne rossa
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
