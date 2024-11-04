727ada367e3ec270df51462f37ae47fd4e82aa48
antoniochiostro <antonio.chiostro@capgemini.com>
2024-11-04 16:38:12 +0100

    add fix

--- src/pages/ImpegniSettimanali.js
+++ src/pages/ImpegniSettimanali.js
@@ -4,7 +4,7 @@ function ImpegniSettimanali() {
   return (
     <div>
       <h1>Impegni Settimanali</h1>
-      <p>Qui verranno mostrati i tuoi impegni settimanali.</p>
+      <p>Qui verranno mostrati tutti tuoi impegni settimanali.</p>
     </div>
   );
 }
